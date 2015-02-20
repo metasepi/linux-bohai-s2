a74a4eb011561f815f70b9d9d74a18a23709ccd1
Kiwamu Okabe <kiwamu@debian.or.jp>
2015-02-20 11:22:45 +0900

    Add script to create orgheader

---
+++ metasepi/tools/showorgheader.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+simplepath () {
+    REALPATH=`realpath $1`
+    awk "BEGIN { str = \"${REALPATH}\"; sub(\"${PWD}/\", \"\", str); print str }"
+}
+
+SIMPLEPATH=`simplepath $1`
+GITLOG=`git log ${SIMPLEPATH} | head -1`
+GITCOMMIT=`awk "BEGIN { str = \"${GITLOG}\"; sub(/^commit +/, \"\", str); print str }"`
+
+cat <<EOF
+(*
+ * Original:  ${SIMPLEPATH}
+ * GitCommit: ${GITCOMMIT}
+ *)
+EOF
